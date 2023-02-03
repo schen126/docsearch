a8b08cdd99a23bc265a6340686080ad7896f00b7
schen126 <sec022@ucsd.edu>
2023-02-03 11:11:36 -0800

    Lab 3 work

---
+++ count-txts.sh
@@ -0,0 +1,7 @@
+find $1 > find-results.txt
+grep ".txt" find-results.txt > grep-results.txt
+wc grep-results.txt
+
+#find written_2 > find-results.txt
+#grep ".txt" find-results.txt > grep-results.txt
+#wc grep-results.txt
